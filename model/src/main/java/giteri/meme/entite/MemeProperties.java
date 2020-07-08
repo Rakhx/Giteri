5c7d7f407292fb10f168c61af792307dd92dd7ac
Rakhx <audren.bouadjio@gmail.com>
2020-07-08 14:09:16 +0200

    Se prépare a faire de meilleur indicateur et a essayer de faire marcher le truc

--- model/src/main/java/giteri/meme/entite/MemeProperties.java
+++ model/src/main/java/giteri/meme/entite/MemeProperties.java
@@ -23,6 +23,7 @@ public class MemeProperties {
 
     // Nombre d'appel depuis le début de la simu
     private Map<IUnitOfTransfer, Integer> nbActivationByMemes;
+    private Map<IUnitOfTransfer, Integer> nbActivationByCouple;
 
     // Compte sur les X derniers appels des memes
     Map<IUnitOfTransfer, Integer> countOfLastMemeActivation;
@@ -47,6 +48,7 @@ public class MemeProperties {
 
      MemeProperties(){
         nbActivationByMemes = new Hashtable<>();
+        nbActivationByCouple = new Hashtable<>();
         countOfLastMemeActivation = new Hashtable<>();
         countOfEntitiesHavingMeme = new Hashtable<>();
         lastHundredActionDone = new CircularFifoQueue<>(Configurator.sizeOfCircularForLastActionDone);
@@ -56,6 +58,7 @@ public class MemeProperties {
 
     public void clear(){
         nbActivationByMemes.clear();
+        nbActivationByCouple.clear();
         countOfLastMemeActivation.clear();
         lastHundredActionDone.clear();
         countOfEntitiesHavingMeme.clear();
@@ -75,12 +78,13 @@ public class MemeProperties {
      * @param cptModulo
      * @return Une liste décrivant les échecs en détails, null sinon.
      */
-    List<String> updateActionCount(Meme memeApply, int entiteIndex, String message, int cptModulo){
+    List<String> updateActionCount(Meme memeApply, IUnitOfTransfer fromCouple, int entiteIndex, String message, int cptModulo){
         // SUCCES: En cas de réussite de l'action
         if (memeApply != null && !message.contains("Nope"))
         {
             Interfaces.IUnitOfTransfer elementRemoveOfCircular = null;
             Toolz.addCountToElementInHashArray(nbActivationByMemes, memeApply, 1);
+            Toolz.addCountToElementInHashArray(nbActivationByCouple, fromCouple, 1);
             synchronized (lastHundredActionDone) {
                 // partie last twenty
                 if (lastHundredActionDone.size() == lastHundredActionDone.maxSize()) {
@@ -88,9 +92,10 @@ public class MemeProperties {
                     Toolz.removeCountToElementInHashArray(countOfLastMemeActivation, elementRemoveOfCircular, 1);
                 }
 
-                lastHundredActionDone.add(memeApply);
+                lastHundredActionDone.add(fromCouple);
             }
-            Toolz.addCountToElementInHashArray(countOfLastMemeActivation, memeApply, 1);
+
+            Toolz.addCountToElementInHashArray(countOfLastMemeActivation, fromCouple, 1);
         }
         // ECHEC: Dans le cas ou il n'y a pas de meme apply, c'est a dire que l'action d'application du meme à échouée.
         // Ne contient que de l'affichage et erciture.
@@ -146,7 +151,7 @@ public class MemeProperties {
 
     // region Ecriture dans fichier
 
-    /**
+    /** Header pour le fichier
      *
      * @param param
      * @param forRepetition si pour répetition, valeur de la répétition, sinon, celui des répétitions du runs
@@ -174,6 +179,13 @@ public class MemeProperties {
         return header;
     }
 
+    /**
+     *
+     * @param param
+     * @param memeCombinaisonFittingAvailable
+     * @param forRepetition
+     * @return
+     */
     public String getStringHeaderCombinaison(List<IModelParameter<?>> param, Map<Integer, ArrayList<Meme>> memeCombinaisonFittingAvailable,boolean forRepetition){
         String header = "Run-Rep";
         for (IModelParameter<?> model :param)
@@ -202,7 +214,6 @@ public class MemeProperties {
         return header;
     }
 
-
     /** création du header pour le fichier de detail concernant les memes sur l
      * la simulation.
      *  Ligne 1: meme
@@ -214,7 +225,7 @@ public class MemeProperties {
      * @param explorator
      * @return
      */
-    public List<String> getHeaderToWriteMemeDetails(Map<Meme, String> translation , int numeroRun, int numeroRep, IExplorationMethod explorator){
+    public List<String> getHeaderToWriteMemeDetails(Map<IUnitOfTransfer, String> translation , int numeroRun, int numeroRep, IExplorationMethod explorator){
         List<String> lines = new ArrayList<>();
         String toWrite ="";
         String line2= "";
@@ -226,7 +237,7 @@ public class MemeProperties {
 //            toWrite += ";" + model.valueString();
         lines.add(toWrite);
         toWrite = "";
-        for (Meme meme : translation.keySet()) {
+        for (IUnitOfTransfer meme : translation.keySet()) {
             toWrite += translation.get(meme);
             line2 += "#possession;";
             toWrite += ";";
@@ -242,11 +253,16 @@ public class MemeProperties {
         return lines;
     }
 
+    /** Ecrire les détails en eu meme sur les UOT
+     *
+     * @param translation
+     * @return
+     */
     public String getStringToWriteMemeDetails(Map<IUnitOfTransfer, String> translation){
         String toWrite ="";
         for (IUnitOfTransfer<CoupleMeme> cMeme : translation.keySet()) {
             toWrite += countOfEntitiesHavingMeme.get(cMeme) + ";";
-            toWrite += nbActivationByMemes.get(cMeme) + ";";
+            toWrite += nbActivationByCouple.get(cMeme) + ";";
             toWrite += countOfLastMemeActivation.get(cMeme) + ";";
         }
 
@@ -255,8 +271,12 @@ public class MemeProperties {
         return toWrite;
     }
 
-
-    public List<String> getCloserToWriteMemeDetails(Map<Meme, String> translation){//} , int numeroRun, int numeroRep, IExplorationMethod explorator){
+    /** a ecrire a la fin du fichier pour des formules de calcul excel
+     *
+     * @param translation
+     * @return
+     */
+    public List<String> getCloserToWriteMemeDetails(Map<IUnitOfTransfer, String> translation){//} , int numeroRun, int numeroRep, IExplorationMethod explorator){
 
         List<String> retur= new ArrayList<>();
         String toWrite ="";
@@ -265,7 +285,7 @@ public class MemeProperties {
         char a = 'a';
         String base = "";
         String colonne;
-        for (Meme meme : translation.keySet()) {
+        for (IUnitOfTransfer meme : translation.keySet()) {
             for (int i = 0; i < 3; i++) {
                 colonne = base + a;
                 a++;
@@ -292,6 +312,10 @@ public class MemeProperties {
 
     Map<IUnitOfTransfer, Integer> getNbActivationByMemes() {
         return nbActivationByMemes;
+    }
+
+    Map<IUnitOfTransfer, Integer> getNbActivationByCouple() {
+        return nbActivationByCouple;
     }
 
 }
